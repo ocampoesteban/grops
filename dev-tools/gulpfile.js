aeb826b162e5696680dbc02a43e471058b8dc54a
steve <estebanocampo21@gmail.com>
2020-10-04 22:13:44 -0300

    WIP

--- dev-tools/gulpfile.js
+++ dev-tools/gulpfile.js
@@ -1,34 +1,38 @@
-// --------------------------------------------------
-// [Gulpfile]
-// --------------------------------------------------
+// // --------------------------------------------------
+// // [Gulpfile]
+// // --------------------------------------------------
 
 'use strict';
  
 var gulp 		= require('gulp'),
 	sass 		= require('gulp-sass'),
-	changed 	= require('gulp-changed'),
-	cleanCSS 	= require('gulp-clean-css'),
-	rtlcss 		= require('gulp-rtlcss'),
-	rename 		= require('gulp-rename'),
-	uglify 		= require('gulp-uglify'),
-	pump 		= require('pump'),
-	htmlhint  	= require('gulp-htmlhint');
+	changed 	= require('gulp-changed');
+	// cleanCSS 	= require('gulp-clean-css'),
+	// rtlcss 		= require('gulp-rtlcss'),
+	// rename 		= require('gulp-rename'),
+	// uglify 		= require('gulp-uglify'),
+	// pump 		= require('pump'),
+	// htmlhint  	= require('gulp-htmlhint');
+
+gulp.task('started', function() {
+	console.log('Gulp started');
+});
+	
+
+// // Gulp plumber error handler
+// function errorLog(error) {
+// 	console.error.bind(error);
+// 	this.emit('end');
+// }
 
 
-// Gulp plumber error handler
-function errorLog(error) {
-	console.error.bind(error);
-	this.emit('end');
-}
-
-
-// --------------------------------------------------
-// [Libraries]
-// --------------------------------------------------
+// // --------------------------------------------------
+// // [Libraries]
+// // --------------------------------------------------
 
 // Sass - Compile Sass files into CSS
 gulp.task('sass', function () {
-	gulp.src('../HTML/sass/**/*.scss')
+	return gulp.src('../HTML/sass/**/*.scss')
 		.pipe(changed('../HTML/css/'))
 		.pipe(sass({ outputStyle: 'expanded' }))
 		.on('error', sass.logError)
@@ -36,72 +40,83 @@ gulp.task('sass', function () {
 });
 
 
-// Minify CSS
-gulp.task('minify-css', function() {
-	// Theme
-    gulp.src(['../HTML/css/layout.css', '!../HTML/css/layout.min.css'])
-        .pipe(cleanCSS({debug: true}, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../HTML/css/'));
+// // // Minify CSS
+// // gulp.task('minify-css', function() {
+// // 	// Theme
+// //     gulp.src(['../HTML/css/layout.css', '!../HTML/css/layout.min.css'])
+// //         .pipe(cleanCSS({debug: true}, function(details) {
+// //             console.log(details.name + ': ' + details.stats.originalSize);
+// //             console.log(details.name + ': ' + details.stats.minifiedSize);
+// //         }))
+// //         .pipe(rename({suffix: '.min'}))
+// //         .pipe(gulp.dest('../HTML/css/'));
 
-    // RTL
-    gulp.src(['../HTML/css/layout-rtl.css', '!../HTML/css/layout-rtl.min.css'])
-        .pipe(cleanCSS({debug: true}, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../HTML/css/'));
-});
-
-
-// RTL CSS - Convert LTR CSS to RTL.
-gulp.task('rtlcss', function () {
-	gulp.src(['../HTML/css/layout.css', '!../HTML/css/layout.min.css', '!../HTML/css/layout-rtl.css', '!../HTML/css/layout-rtl.min.css'])
-	.pipe(changed('../HTML/css/'))
-		.pipe(rtlcss())
-		.pipe(rename({ suffix: '-rtl' }))
-		.pipe(gulp.dest('../HTML/css/'));
-});
+// //     // RTL
+// //     gulp.src(['../HTML/css/layout-rtl.css', '!../HTML/css/layout-rtl.min.css'])
+// //         .pipe(cleanCSS({debug: true}, function(details) {
+// //             console.log(details.name + ': ' + details.stats.originalSize);
+// //             console.log(details.name + ': ' + details.stats.minifiedSize);
+// //         }))
+// //         .pipe(rename({suffix: '.min'}))
+// //         .pipe(gulp.dest('../HTML/css/'));
+// // });
 
 
-// Minify JS - Minifies JS
-gulp.task('uglify', function (cb) {
-  	pump([
-	        gulp.src(['../HTML/js/**/*.js', '!../HTML/js/**/*.min.js']),
-	        uglify(),
-			rename({ suffix: '.min' }),
-	        gulp.dest('../HTML/js/')
-		],
-		cb
-	);
-});
+// // // RTL CSS - Convert LTR CSS to RTL.
+// // gulp.task('rtlcss', function () {
+// // 	gulp.src(['../HTML/css/layout.css', '!../HTML/css/layout.min.css', '!../HTML/css/layout-rtl.css', '!../HTML/css/layout-rtl.min.css'])
+// // 	.pipe(changed('../HTML/css/'))
+// // 		.pipe(rtlcss())
+// // 		.pipe(rename({ suffix: '-rtl' }))
+// // 		.pipe(gulp.dest('../HTML/css/'));
+// // });
 
 
-// Htmlhint - Validate HTML
-gulp.task('htmlhint', function() {
-	gulp.src('../HTML/*.html')
-		.pipe(htmlhint())
-		.pipe(htmlhint.reporter())
-	  	.pipe(htmlhint.failReporter({ suppress: true }))
-});
+// // // Minify JS - Minifies JS
+// // gulp.task('uglify', function (cb) {
+// //   	pump([
+// // 	        gulp.src(['../HTML/js/**/*.js', '!../HTML/js/**/*.min.js']),
+// // 	        uglify(),
+// // 			rename({ suffix: '.min' }),
+// // 	        gulp.dest('../HTML/js/')
+// // 		],
+// // 		cb
+// // 	);
+// // });
 
 
-// --------------------------------------------------
-// [Gulp Task - Watch]
-// --------------------------------------------------
+// // // Htmlhint - Validate HTML
+// // gulp.task('htmlhint', function() {
+// // 	gulp.src('../HTML/*.html')
+// // 		.pipe(htmlhint())
+// // 		.pipe(htmlhint.reporter())
+// // 	  	.pipe(htmlhint.failReporter({ suppress: true }))
+// // });
 
-// Lets us type "gulp" on the command line and run all of our tasks
-gulp.task('default', ['sass', 'minify-css', 'rtlcss', 'uglify', 'htmlhint', 'watch']);
+
+// // --------------------------------------------------
+// // [Gulp Task - Watch]
+// // --------------------------------------------------
+
+// // This handles watching and running tasks
+// gulp.task('watch', function () {
+//     gulp.watch('../HTML/sass/**/*.scss', ['sass']);
+//     // gulp.watch('../HTML/css/layout.css', ['minify-css']);
+//     // gulp.watch('../HTML/css/layout.css', ['rtlcss']);
+//     // gulp.watch('../HTML/js/**/*.js', ['uglify']);
+//     // gulp.watch('../HTML/*.html', ['htmlhint']);
+// });
 
 // This handles watching and running tasks
 gulp.task('watch', function () {
     gulp.watch('../HTML/sass/**/*.scss', ['sass']);
-    gulp.watch('../HTML/css/layout.css', ['minify-css']);
-    gulp.watch('../HTML/css/layout.css', ['rtlcss']);
-    gulp.watch('../HTML/js/**/*.js', ['uglify']);
-    gulp.watch('../HTML/*.html', ['htmlhint']);
+    // gulp.watch('../HTML/css/layout.css', ['minify-css']);
+    // gulp.watch('../HTML/css/layout.css', ['rtlcss']);
+    // gulp.watch('../HTML/js/**/*.js', ['uglify']);
+    // gulp.watch('../HTML/*.html', ['htmlhint']);
 });
+
+// Lets us type "gulp" on the command line and run all of our tasks
+// gulp.task('default', ['sass', 'minify-css', 'rtlcss', 'uglify', 'htmlhint', 'watch']);
+
+gulp.task('default', gulp.series('started', 'sass' ));
